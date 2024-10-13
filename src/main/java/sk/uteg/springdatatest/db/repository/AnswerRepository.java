936be5c119fb4a3448a5846d28f2f81930918827
Roman Micuda <roman.micuda@outlook.com>
2024-10-13 21:17:49 +0200

    refactor(answer): update method to find answers by question ID instead of question object

--- src/main/java/sk/uteg/springdatatest/db/repository/AnswerRepository.java
+++ src/main/java/sk/uteg/springdatatest/db/repository/AnswerRepository.java
@@ -1,12 +1,11 @@
 package sk.uteg.springdatatest.db.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import sk.uteg.springdatatest.db.model.Question;
 import sk.uteg.springdatatest.db.model.Answer;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface AnswerRepository extends JpaRepository<Answer, UUID> {
-    List<Answer> findAllByQuestion(Question question);
+    List<Answer> findAllByQuestionId(UUID questionId);
 }
