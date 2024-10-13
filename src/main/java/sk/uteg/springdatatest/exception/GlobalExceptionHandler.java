51b995c5e3f1155e606c028479a13a92a5e00bb0
Roman Micuda <roman.micuda@outlook.com>
2024-10-13 21:12:08 +0200

    refactor(exception): rename ControllerConflictException to GlobalExceptionHandler

--- src/main/java/sk/uteg/springdatatest/exception/GlobalExceptionHandler.java
+++ src/main/java/sk/uteg/springdatatest/exception/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class ControllerConflictException {
+public class GlobalExceptionHandler {
     
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
