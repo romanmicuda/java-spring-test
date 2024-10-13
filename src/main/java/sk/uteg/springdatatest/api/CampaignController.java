db9691804c13520196c8fc043c46cadeebd84275
Roman Micuda <roman.micuda@outlook.com>
2024-10-13 21:23:34 +0200

    feat(api): refactor CampaignController to use ICampaignService

--- src/main/java/sk/uteg/springdatatest/api/CampaignController.java
+++ src/main/java/sk/uteg/springdatatest/api/CampaignController.java
@@ -1,18 +1,27 @@
 package sk.uteg.springdatatest.api;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sk.uteg.springdatatest.api.model.CampaignSummary;
+import sk.uteg.springdatatest.exception.IllegalOperationException;
+import sk.uteg.springdatatest.exception.NotFoundException;
+import sk.uteg.springdatatest.service.ICampaignService;
 
 import java.util.UUID;
 
-@RestController("campaign")
+@RestController
+@RequestMapping("/campaign")
+@RequiredArgsConstructor
 public class CampaignController {
 
+    private final ICampaignService campaignService;
+
     @GetMapping("/summary/{uuid}")
-    public ResponseEntity<CampaignSummary> getSummary(@PathVariable UUID uuid) {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<CampaignSummary> getCampaignSummary(@PathVariable UUID uuid) 
+            throws NotFoundException, IllegalOperationException {
+        return ResponseEntity.ok(campaignService.getCampaignSummary(uuid));
+
     }
+
 }
